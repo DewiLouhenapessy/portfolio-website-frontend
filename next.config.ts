1c6d428ae2de0a2271712c153966d47476bd7c00
Dewi Louhenapessy <dewilouhenapessy@gmail.com>
2026-07-27 20:59:58 +0200

    remove redundant code

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-	i18n: {
-		locales: ["nl", "en"],
-		defaultLocale: "nl",
-		localeDetection: false,
-		domains: [
-			{
-				domain: "project-name.nl",
-				defaultLocale: "nl",
-			},
-			{
-				domain: "project-name.com",
-				defaultLocale: "en",
-			},
-		],
-	},
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
